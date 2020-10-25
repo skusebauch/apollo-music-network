783f576380a5cca902dfc514c7daa22eec331c9e
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-25 18:50:44 +0100

    added song cards and list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,47 @@
-import { CircularProgress } from '@material-ui/core';
 import React from 'react';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  IconButton,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: theme.spacing(3),
+  },
+  songInfoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  songInfo: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  thumbnail: {
+    objectFit: 'cover',
+    width: 140,
+    height: 140,
+  },
+}));
 
 function SongList() {
   let loading = false;
+
+  const song = {
+    title: '- Piano Version - Sag mir wer - mit Kostas Karagiozidis',
+    artist: 'Apache 207',
+    thumbnail:
+      'https://i.ytimg.com/vi/EKuV5gGD9tc/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLApUWlOBh9Q_HS1VJqPEW8jGjnKXw',
+  };
+
   if (loading) {
     return (
       <div
@@ -20,8 +59,45 @@ function SongList() {
 
   return (
     <div>
-      <p>Song List component</p>
+      {Array.from({ length: 10 }, () => song).map((song, index) => (
+        <Song key={index} song={song} />
+      ))}
     </div>
+  );
+}
+
+function Song({ song }) {
+  const classes = useStyles();
+  const { title, artist, thumbnail } = song;
+  return (
+    <Card className={classes.container}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.thumbnail} />
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom varaiant='h5' component='h2'>
+              {title}
+            </Typography>
+            <Typography
+              gutterBottom
+              varaiant='body1'
+              component='p'
+              color='textSecondary'
+            >
+              {artist}
+            </Typography>
+          </CardContent>
+        </div>
+        <CardActions>
+          <IconButton size='small' color='primary'>
+            <PlayCircleOutlineIcon />
+          </IconButton>
+          <IconButton size='small' color='primary'>
+            <FavoriteBorderIcon />
+          </IconButton>
+        </CardActions>
+      </div>
+    </Card>
   );
 }
 
